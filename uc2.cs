99c7f96aee8ccbd58c8f902dd72143ed4dcd97a5
Akashkapase9988 <akashkapase9988@gmail.com>
2021-10-23 10:49:08 +0530

    [Akash addded uc2 for player]

---
+++ uc2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workshoptictoc
+{
+    public class uc2
+    {
+        public static int Player()
+        {
+
+            int palyer1 = 1;
+            int player2 = 2;
+            int ch = Convert.ToInt32(Console.ReadLine());
+            if (ch == 1)
+            {
+                Console.WriteLine("X");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
